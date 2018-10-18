7ac4e7fbf4d3176e7756c53856b4ee79590539ad
Aviral Verma <aviral1701@gmail.com>
2018-10-18 14:58:05 +0530

    Initial

---
+++ boot.py
@@ -0,0 +1,24 @@
+import globals
+import threading
+import socket
+
+
+
+def boot_network():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((globals.HOST, globals.PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)
+
+if __name__=="__main__":
+    t1 = threading.Thread(target = boot_network)
+    t1.start()
+    print("hello world")
+    t1.join()
