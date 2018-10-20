48143a7a33035bbc85c2fbe74ac28fca40219473
Aviral Verma <aviral1701@gmail.com>
2018-10-20 20:53:26 +0530

    threaded server working

--- central_server.py
+++ central_server.py
@@ -1,28 +1,42 @@
 import socket 
-import threading 
-import time
-  
-def threaded(c): 
-    time.sleep(5)
+from threading import Thread 
+from SocketServer import ThreadingMixIn 
 
-  
-  
-def Main(): 
-    host = "" 
-    port = 6254
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.bind((host, port)) 
-    s.listen(5) 
-    print("socket is listening") 
-  
-    # a forever loop until client wants to exit 
-    while True: 
-        c, addr = s.accept()
-        print('Connected to :', addr[0], ':', addr[1]) 
-        t1 = threading.Thread(threaded, (c,))
-        t1.start()
-    s.close() 
-  
-  
-if __name__ == '__main__': 
-    Main() 
+# Multithreaded Python server : TCP Server Socket Thread Pool
+class ClientThread(Thread): 
+ 
+    def __init__(self,ip,port): 
+        Thread.__init__(self) 
+        self.ip = ip 
+        self.port = port 
+        print "[+] New server socket thread started for " + ip + ":" + str(port) 
+ 
+    def run(self): 
+        while True : 
+            data = conn.recv(2048) 
+            print "Server received data:", data
+            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
+            if MESSAGE == 'exit':
+                break
+            conn.send(MESSAGE)  # echo 
+
+# Multithreaded Python server : TCP Server Socket Program Stub
+TCP_IP = '0.0.0.0' 
+TCP_PORT = 2004 
+BUFFER_SIZE = 20  # Usually 1024, but we need quick response 
+
+tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+tcpServer.bind((TCP_IP, TCP_PORT)) 
+threads = [] 
+ 
+while True: 
+    tcpServer.listen(4) 
+    print "Multithreaded Python server : Waiting for connections from TCP clients..." 
+    (conn, (ip,port)) = tcpServer.accept() 
+    newthread = ClientThread(ip,port) 
+    newthread.start() 
+    threads.append(newthread) 
+ 
+for t in threads: 
+    t.join() 
