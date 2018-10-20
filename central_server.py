ae4dc34ccb0276613d0079ba64a1b5dbb791484b
Aviral Verma <aviral1701@gmail.com>
2018-10-20 21:43:04 +0530

    threaded server working

--- central_server.py
+++ central_server.py
@@ -1,42 +1,45 @@
-import socket 
-from threading import Thread 
-from SocketServer import ThreadingMixIn 
+import socket
+import threading
 
-# Multithreaded Python server : TCP Server Socket Thread Pool
-class ClientThread(Thread): 
- 
-    def __init__(self,ip,port): 
-        Thread.__init__(self) 
-        self.ip = ip 
-        self.port = port 
-        print "[+] New server socket thread started for " + ip + ":" + str(port) 
- 
-    def run(self): 
-        while True : 
-            data = conn.recv(2048) 
-            print "Server received data:", data
-            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
-            if MESSAGE == 'exit':
-                break
-            conn.send(MESSAGE)  # echo 
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        self.clients = []
 
-# Multithreaded Python server : TCP Server Socket Program Stub
-TCP_IP = '0.0.0.0' 
-TCP_PORT = 2004 
-BUFFER_SIZE = 20  # Usually 1024, but we need quick response 
+    def listen(self):
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            t = threading.Thread(target = self.listenToClient,args = (client,address)).start()
+            self.clients.append(t)
 
-tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-tcpServer.bind((TCP_IP, TCP_PORT)) 
-threads = [] 
- 
-while True: 
-    tcpServer.listen(4) 
-    print "Multithreaded Python server : Waiting for connections from TCP clients..." 
-    (conn, (ip,port)) = tcpServer.accept() 
-    newthread = ClientThread(ip,port) 
-    newthread.start() 
-    threads.append(newthread) 
- 
-for t in threads: 
-    t.join() 
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                if data:
+                    # Set the response to echo back the recieved data 
+                    response = data
+                    client.send(response)
+                else:
+                    raise error('Client disconnected')
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    while True:
+        port_num = input("Port? ")
+        try:
+            port_num = int(port_num)
+            break
+        except ValueError:
+            pass
+
+    ThreadedServer('',port_num).listen()
--- clientB.py
+++ clientB.py
@@ -6,7 +6,7 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 6254      # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('', 2018))
+    s.bind(('', 2019))
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
     data = s.recv(1024)
